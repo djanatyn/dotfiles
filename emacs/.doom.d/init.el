76e3b32190d1cf5c567a5f464362522a4a249ca0
Jonathan Strickland <djanatyn@gmail.com>
2022-05-29 20:26:51 -0400

    emacs: zig, ruby, plantuml, lua

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -10,6 +10,7 @@
  :editor snippets (evil +everywhere) file-templates (format +onsave) parinfer
  :emacs dired electric fold vc (undo +tree)
  :tools lookup eval editorconfig make magit pass tmux lsp
- :lang elm dhall lua data (haskell +lsp) markdown (org +roam +dragndrop +pandoc) (rust +lsp) (sh +zsh) racket yaml nix julia common-lisp
+ :lang elm dhall lua data (haskell +lsp) markdown (org +roam +dragndrop +pandoc)
+ (rust +lsp) (sh +zsh) racket yaml nix julia common-lisp plantuml zig ruby lua
  :term vterm
  :config (default +bindings +snippets +evil-commands))
