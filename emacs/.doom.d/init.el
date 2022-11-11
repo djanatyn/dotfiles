3f45100d39d680034d66c674b161449db6be8023
Jonathan Strickland <djanatyn@gmail.com>
2022-11-11 15:34:41 -0500

    emacs: add notmuch+web

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -7,10 +7,11 @@
  nav-flash (popup +all +defaults) ligatures vc-gutter vi-tilde-fringe
  window-select treemacs hydra zen minimap workspaces
 
+ :email notmuch
  :editor snippets (evil +everywhere) file-templates (format +onsave) parinfer
  :emacs dired electric fold vc (undo +tree)
  :tools lookup eval editorconfig make magit pass tmux lsp
- :lang elm dhall lua data (haskell +lsp) markdown (org +roam +dragndrop +pandoc)
+ :lang elm dhall lua data (haskell +lsp) markdown (org +roam +dragndrop +pandoc) (web +html)
  (rust +lsp) (sh +zsh) racket yaml nix julia common-lisp plantuml zig ruby lua
  :term vterm
  :config (default +bindings +snippets +evil-commands))
